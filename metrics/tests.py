bb48e5ce120886d69e814891c8e9d1297299aeb0
Nikolay Denisenko <mail2nikolas@gmail.com>
2017-06-15 16:47:58 +0300

    Updated tests

--- metrics/tests.py
+++ metrics/tests.py
@@ -2,7 +2,7 @@ from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
 
-from .models import Activity
+from .models import Activity, Location
 
 class ActivityTests(APITestCase):
     @classmethod
@@ -43,3 +43,45 @@ class ActivityTests(APITestCase):
                                       format='json')
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
 
+
+class LocationTests(APITestCase):
+    @classmethod
+    def setUpTestData(cls):
+        Location.objects.create(
+            time_start='2017-06-15T10:00:00Z',
+            time_end='2017-06-15T11:00:00Z',
+            latitude='55.7536207',
+            longitude='37.6225599',
+        )
+
+    def test_create_activity(self):
+        response = self.client.post(reverse('metrics:location-list'), {
+            'time_start': '2017-06-15T11:00:00Z',
+            'time_end': '2017-06-15T12:00:00Z',
+            'latitude': '55.7536207',
+            'longitude': '37.6225599',
+        }, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_list_activity(self):
+        response = self.client.get(reverse('metrics:location-list'), format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_retrieve_activity(self):
+        response = self.client.get(reverse('metrics:location-detail', kwargs={'pk': 1}),
+                                   format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_update_activity(self):
+        response = self.client.put(reverse('metrics:location-detail', kwargs={'pk': 1}), {
+            'time_start': '2017-06-15T10:00:00Z',
+            'time_end': '2017-06-15T11:00:00Z',
+            'latitude': '37.6225599',
+            'longitude': '55.7536207',
+        }, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_destroy_activity(self):
+        response = self.client.delete(reverse('metrics:location-detail', kwargs={'pk': 1}),
+                                      format='json')
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
